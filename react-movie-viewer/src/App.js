ada47092bf412ff0fd44bfba71ba5d1f7872e126
Juhwan <dev.juhwanlee@gmail.com>
2022-04-07 01:46:25 +0900

    Add code: useEffect

--- react-movie-viewer/src/App.js
+++ react-movie-viewer/src/App.js
@@ -1,9 +1,12 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 function App() {
   const [counter, setValue] = useState(0);
   const onClick = () => setValue((prev) => prev + 1);
-  console.log('render');
+  console.log('i run all the time');
+  useEffect(() => {
+    console.log('call the API...');
+  }, []);
   return (
     <div>
       <h1>{counter}</h1>
