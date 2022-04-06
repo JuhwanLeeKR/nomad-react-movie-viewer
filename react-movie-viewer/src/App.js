3d51519b13791c8d4b401ae66aace2aac721910c
Juhwan <dev.juhwanlee@gmail.com>
2022-04-07 00:05:45 +0900

    Create files: css module

--- react-movie-viewer/src/App.js
+++ react-movie-viewer/src/App.js
@@ -1,9 +1,10 @@
 import Button from './Button';
+import styles from './App.module.css';
 
 function App() {
   return (
     <div>
-      <h1>Welcom back!</h1>
+      <h1 className={styles.title}>Welcom back!</h1>
       <Button text={'Continue'} />
     </div>
   );
--- react-movie-viewer/src/Button.js
+++ react-movie-viewer/src/Button.js
@@ -1,4 +1,12 @@
+import PropTypes from 'prop-types';
+import styles from './Button.module.css';
+
 function Button({ text }) {
-  return <button>{text}</button>;
+  return <button className={styles.btn}>{text}</button>;
 }
+
+Button.propTypes = {
+  text: PropTypes.string.isRequired,
+};
+
 export default Button;
