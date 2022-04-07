dca99745ac95edf2dd7bcaddf653972f8f8c75d7
Juhwan <dev.juhwanlee@gmail.com>
2022-04-07 11:34:18 +0900

    Publishing with package.json setting: gh-pages, deploy, predeploy, homepage.etc

--- react-movie-viewer/src/App.js
+++ react-movie-viewer/src/App.js
@@ -9,7 +9,7 @@ function App() {
         <Route path='/hello'>
           <h1>Hello</h1>
         </Route>
-        <Route path='/movie'>
+        <Route path='/movie/:id'>
           <Detail />
         </Route>
         <Route path='/'>
--- react-movie-viewer/src/routes/Home.js
+++ react-movie-viewer/src/routes/Home.js
@@ -1,6 +1,8 @@
 import { useEffect, useState } from 'react';
 import Movie from '../components/Movie';
 
+import styles from './Home.module.css';
+
 function Home() {
   const [loading, setLoading] = useState(true);
   const [movies, setMovies] = useState([]);
@@ -17,14 +19,18 @@ function Home() {
     getMovies();
   }, []);
   return (
-    <div>
+    <div className={styles.container}>
       {loading ? (
-        <h1>Loading...</h1>
+        <div className={styles.loader}>
+          <span>Loading...</span>
+        </div>
       ) : (
-        <div>
+        <div className={styles.movies}>
           {movies.map((movie) => (
             <Movie
               key={movie.id}
+              id={movie.id}
+              year={movie.year}
               coverImg={movie.medium_cover_image}
               title={movie.title}
               summary={movie.summary}
