5ca319da570b069fbd4720e5ad4f72b4392e0bea
Juhwan <dev.juhwanlee@gmail.com>
2022-04-07 09:06:34 +0900

    Movie app: use react-router-dom@5.3.0

--- react-movie-viewer/src/App.js
+++ react-movie-viewer/src/App.js
@@ -1,31 +1,22 @@
-import { useEffect, useState } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Detail from './routes/Detail';
+import Home from './routes/Home';
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
-  useEffect(() => {
-    fetch('https://api.coinpaprika.com/v1/tickers')
-      .then((response) => response.json())
-      .then((json) => {
-        setCoins(json);
-        setLoading(false);
-      });
-  }, []);
   return (
-    <div>
-      <h1>The Coins! {loading ? '' : `(${coins.length})`}</h1>
-      {loading ? (
-        <strong>Loading...</strong>
-      ) : (
-        <select>
-          {coins.map((coin) => (
-            <option key={coin.id}>
-              {coin.name} ({coin.symbol}): ${coin.quotes.USD.price} USD
-            </option>
-          ))}
-        </select>
-      )}
-    </div>
+    <Router>
+      <Switch>
+        <Route path='/hello'>
+          <h1>Hello</h1>
+        </Route>
+        <Route path='/movie'>
+          <Detail />
+        </Route>
+        <Route path='/'>
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
